1e50bbea951ba46774ac419ad4c7511253bfcef6
Krzysztof Warunek <krzysztof@warunek.net>
2017-08-17 16:30:33 +0200

    simplify

--- aiounittest/case.py
+++ aiounittest/case.py
@@ -50,87 +50,25 @@ class AsyncTestCase(unittest.TestCase):
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-    def _runTestMethod(self, testMethod):
-        loop = self.get_event_loop()
-        ret = testMethod()
-        try:
-            future = asyncio.ensure_future(ret, loop=loop)
-        except TypeError:
-            # The test function is not awaitable.
-            # Using `try/except` rather than iscoroutine due to support of 3.4 
-            pass
+
+    def __getattribute__(self, name):
+        attr = super().__getattribute__(name)
+        if name.startswith('test_'):  # and isinstance(attr, unction):
+            return self._runTestMethod(attr)
         else:
-            loop.run_until_complete(future)
+            return attr
 
-    def run(self, result=None):
-        orig_result = result
-        if result is None:
-            result = self.defaultTestResult()
-            startTestRun = getattr(result, 'startTestRun', None)
-            if startTestRun is not None:
-                startTestRun()
-
-        result.startTest(self)
-
-        testMethod = getattr(self, self._testMethodName)
-        if (getattr(self.__class__, "__unittest_skip__", False) or
-                getattr(testMethod, "__unittest_skip__", False)):
-            # If the class or method was skipped.
+    def _runTestMethod(self, testMethod):
+        @functools.wraps(testMethod)
+        def wrapped(*args, **kwargs):
+            loop = self.get_event_loop()
+            ret = testMethod(*args, **kwargs)
             try:
-                skip_why = (
-                    getattr(self.__class__, '__unittest_skip_why__', '') or
-                    getattr(testMethod, '__unittest_skip_why__', '')
-                )
-                self._addSkip(result, self, skip_why)
-            finally:
-                result.stopTest(self)
-            return
-        expecting_failure_method = getattr(testMethod,
-                                           "__unittest_expecting_failure__", False)
-        expecting_failure_class = getattr(self,
-                                          "__unittest_expecting_failure__", False)
-        expecting_failure = expecting_failure_class or expecting_failure_method
-        outcome = _Outcome(result)
-        try:
-            self._outcome = outcome
-
-            with outcome.testPartExecutor(self):
-                self.setUp()
-
-            if outcome.success:
-                outcome.expecting_failure = expecting_failure
-                with outcome.testPartExecutor(self, isTest=True):
-                    self._runTestMethod(testMethod)
-                outcome.expecting_failure = False
-
-                with outcome.testPartExecutor(self):
-                    self.tearDown()
-
-            self.doCleanups()
-            for test, reason in outcome.skipped:
-                self._addSkip(result, test, reason)
-            self._feedErrorsToResult(result, outcome.errors)
-            if outcome.success:
-                if expecting_failure:
-                    if outcome.expectedFailure:
-                        self._addExpectedFailure(result, outcome.expectedFailure)
-                    else:
-                        self._addUnexpectedSuccess(result)
-                else:
-                    result.addSuccess(self)
-            return result
-        finally:
-            result.stopTest(self)
-            if orig_result is None:
-                stopTestRun = getattr(result, 'stopTestRun', None)
-                if stopTestRun is not None:
-                    stopTestRun()
-
-            # explicitly break reference cycles:
-            # outcome.errors -> frame -> outcome -> outcome.errors
-            # outcome.expectedFailure -> frame -> outcome -> outcome.expectedFailure
-            outcome.errors.clear()
-            outcome.expectedFailure = None
-
-            # clear the outcome, no more needed
-            self._outcome = None
+                future = asyncio.ensure_future(ret, loop=loop)
+            except TypeError:
+                # The test function is not awaitable.
+                # Using `try/except` rather than iscoroutine due to support of 3.4 
+                pass
+            else:
+                loop.run_until_complete(future)
+        return wrapped
