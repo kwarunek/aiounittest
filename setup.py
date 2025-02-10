0f3d21c0874ea3d931ed33e5ae3a6dd1278be7df
Krzysztof Warunek <krzysztof@warunek.net>
2025-02-10 13:17:56 +0100

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 setup(
     name='aiounittest',
     packages=['aiounittest'],
-    version='1.4.2',
+    version='1.4.3',
     author='Krzysztof Warunek',
     author_email='krzysztof@warunek.net',
     description='Test asyncio code more easily.',
